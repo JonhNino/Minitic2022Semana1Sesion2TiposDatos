b152b55406bb21ff2a72a06956665cb13740f227
Ferney Niño <Ferney Nio@DESKTOP-91UIOGN>
2022-07-20 23:31:57 -0500

    20/07/2022 Semana1 Sesion2 Mintic 2022 Java Tipos de Datos

---
+++ src/tiposdedatos/TiposdeDatos.java
@@ -0,0 +1,37 @@
+
+package tiposdedatos;
+
+
+public class TiposdeDatos {
+
+   
+    public static void main(String[] args) {
+        byte var_byte;
+        var_byte=120;
+        System.out.println("Variable byte: "+var_byte);
+        
+        long var_long=15000;
+        System.out.println("Variable Long: "+var_long);
+        
+        double var_double=145.8;
+        System.out.println("Variable Double: "+var_double);
+   
+        float var_float=(float) 56.4;
+        float var_float1=5.9f;
+        System.out.println("Variables Float: "+var_float+"-"+var_float1);
+        
+        String nombre="Jonh Niño";
+        System.out.println("Mi nombre es: "+nombre);
+        //Comentario
+        /*Cp,emtarop artas ñ
+        liestan 
+        diasd
+        diasdi
+         
+        */
+        int var_entera=0;
+        var_entera++;
+        System.out.println("Variable Entera Incrementada "+var_entera);
+    }
+    
+}
